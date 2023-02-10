bfd1f379d92b74bfe923f979c8ebf6ad5a52e699
wilson.gonzalez <wilson.gonzalez.ext@itau.co>
2023-02-10 18:27:03 -0500

    Update the view to show the correct information

--- src/app/components/cards/card-table/card-table.component.ts
+++ src/app/components/cards/card-table/card-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-table',
@@ -6,6 +6,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardTableComponent implements OnInit {
   visibleDetail = false;
+  selectionIndex = 2;
+  productsTmp = [
+  ]
+
   product = [
     {
       product: 'Coca Cola',
@@ -14,149 +18,170 @@ export class CardTableComponent implements OnInit {
       supplier: 'Coca Cola',
       importer: 'N/A',
       amount: 10
-  },
-  {
-    product: 'Coca Cola',
-    imageProduct: 'assets/img/bootstrap.jpg',
-    store: 'Andino',
-    supplier: 'Coca Cola',
-    importer: 'N/A',
-    amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-},
-{
-  product: 'Coca Cola',
-  imageProduct: 'assets/img/bootstrap.jpg',
-  store: 'Andino',
-  supplier: 'Coca Cola',
-  importer: 'N/A',
-  amount: 10
-}]
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    },
+    {
+      product: 'Coca Cola',
+      imageProduct: 'assets/img/bootstrap.jpg',
+      store: 'Andino',
+      supplier: 'Coca Cola',
+      importer: 'N/A',
+      amount: 10
+    }
+    ]
 
   @Input()
   get color(): string {
     return this._color;
   }
+
   set color(color: string) {
     this._color = color !== 'light' && color !== 'dark' ? 'light' : color;
   }
+
   private _color = 'light';
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor() {
+  }
 
-  checkDetailProduct(){
+  ngOnInit(): void {
+    this.getProductsTmp();
+  }
+
+  getProductsTmp(): void {
+    for (let i = 0; i < 10; i++) {
+      if(this.productsTmp.length > 4){
+          break;
+      } else {
+         this.productsTmp.push(this.product[i]);
+      }
+    }
+  }
+
+  getVisibleItemPagination(index): boolean {
+    return this.product.length / index  >= 1;
+  }
+
+  checkDetailProduct() {
     this.visibleDetail = true;
   }
 }
