ff584e28368a938b9adcf838e49397d0772553a5
Alejandro Gonzalez <wilson1234567891962@hotmail.com>
2023-02-16 22:10:05 -0500

    Change the pagination

--- src/app/components/cards/card-table/card-table.component.ts
+++ src/app/components/cards/card-table/card-table.component.ts
@@ -73,7 +73,7 @@ export class CardTableComponent implements OnInit{
   goItemPagination(count, data){
     this.visibleDetail = false;
     data = !this.searchIsVisible ? this.product : this.productSearch;
-    const paginate = this.paginate(data.length, count, 5, 5);
+    const paginate = this.utilitiesService.paginate(data.length, count, 5, 5);
     if(count < 1 || count > paginate.totalPages ){
       return;
     }
@@ -108,67 +108,6 @@ export class CardTableComponent implements OnInit{
   checkDetailProduct(index) {
     this.visibleDetail = true;
     this.detailProduct = !this.searchIsVisible ? this.product[index].detail : this.productSearch[index].detail;
-  }
-
-  paginate(
-    totalItems: number,
-    currentPage: number = 1,
-    pageSize: number = 10,
-    maxPages: number = 10
-  ) {
-    // calculate total pages
-    const totalPages = Math.ceil(totalItems / pageSize);
-
-    // ensure current page isn't out of range
-    if (currentPage < 1) {
-      currentPage = 1;
-    } else if (currentPage > totalPages) {
-      currentPage = totalPages;
-    }
-    let startPage: number;
-    let endPage: number;
-    if (totalPages <= maxPages) {
-      // total pages less than max so show all pages
-      startPage = 1;
-      endPage = totalPages;
-    } else {
-      // total pages more than max so calculate start and end pages
-      const maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
-      const maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
-      if (currentPage <= maxPagesBeforeCurrentPage) {
-        // current page near the start
-        startPage = 1;
-        endPage = maxPages;
-      } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
-        // current page near the end
-        startPage = totalPages - maxPages + 1;
-        endPage = totalPages;
-      } else {
-        // current page somewhere in the middle
-        startPage = currentPage - maxPagesBeforeCurrentPage;
-        endPage = currentPage + maxPagesAfterCurrentPage;
-      }
-    }
-
-    // calculate start and end item indexes
-    const startIndex = (currentPage - 1) * pageSize;
-    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-    // create an array of pages to ng-repeat in the pager control
-    const pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
-
-    // return object with all pager properties required by the view
-    return {
-      totalItems,
-      currentPage,
-      pageSize,
-      totalPages,
-      startPage,
-      endPage,
-      startIndex,
-      endIndex,
-      pages
-    };
   }
 
   search()  {
--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -55,4 +55,66 @@ export class UtilitiesService {
     console.log(inputDataPicker)
     return day === inputDataPicker.getDay() && month === inputDataPicker.getMonth() && year === inputDataPicker.getFullYear();
   }
+
+
+  paginate(
+    totalItems: number,
+    currentPage: number = 1,
+    pageSize: number = 10,
+    maxPages: number = 10
+  ) {
+    // calculate total pages
+    const totalPages = Math.ceil(totalItems / pageSize);
+
+    // ensure current page isn't out of range
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > totalPages) {
+      currentPage = totalPages;
+    }
+    let startPage: number;
+    let endPage: number;
+    if (totalPages <= maxPages) {
+      // total pages less than max so show all pages
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // total pages more than max so calculate start and end pages
+      const maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
+      const maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+      if (currentPage <= maxPagesBeforeCurrentPage) {
+        // current page near the start
+        startPage = 1;
+        endPage = maxPages;
+      } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
+        // current page near the end
+        startPage = totalPages - maxPages + 1;
+        endPage = totalPages;
+      } else {
+        // current page somewhere in the middle
+        startPage = currentPage - maxPagesBeforeCurrentPage;
+        endPage = currentPage + maxPagesAfterCurrentPage;
+      }
+    }
+
+    // calculate start and end item indexes
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+    // create an array of pages to ng-repeat in the pager control
+    const pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+
+    // return object with all pager properties required by the view
+    return {
+      totalItems,
+      currentPage,
+      pageSize,
+      totalPages,
+      startPage,
+      endPage,
+      startIndex,
+      endIndex,
+      pages
+    };
+  }
 }
